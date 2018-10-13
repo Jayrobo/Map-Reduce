848ed681c7ad5b7b1ef6908c4dc6550705416b4c
Jayrobo <ciwang@sfu.ca>
2018-10-12 17:57:42 -0700

    first method almost done

--- Lab 3/Word_Count.cpp
+++ Lab 3/Word_Count.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <unordered_map>
+#include <iterator>
+
 
 using namespace std;
 
 void First_Word_Count()
 {
 	ifstream input_file;
-	string filename, content;
+	string filename;
+	map <string, int> content;
 
 	do
 	{
@@ -19,16 +24,36 @@ void First_Word_Count()
 	} while (input_file.fail());
 
 	int word_count = 0;
+	string delimiter = "\".?,";
+
 	while (!input_file.eof())
 	{
-		getline(input_file, content);
+		//getline(input_file, content);
+		string temp; 
+		input_file >> temp;
+		for (int i = 0; i < temp.size(); ++i)
+		{
+			temp[i] = tolower(temp[i]);
+			if (!((temp[i] >= 'a' && temp[i] <= 'z') || (temp[i] == '\'')))
+			{
+				temp[i] = '\0';
+			}
+		}
+		
+		content.insert(pair<string, int>(temp, 1));
+		//cout << temp << endl;
+		//content.push_back(temp.substr());
 		word_count++;
 	}
 
-	cout << content;
+	for (map<string,int>::iterator it = content.begin(); it != content.end(); it++)
+		cout << it->first << "	 =>  " << it->second << endl;
+	//cout << content << " " << word_count <<endl;
+
 
 	input_file.close();
 }
+
 
 int main()
 {
