19cc67c13c6f66ca0a33c2a7b9b3f8b50463eb78
Jayrobo <ciwang@sfu.ca>
2018-10-14 22:28:44 -0700

    rearrange the files and adding first part of method 2 still some bug with the libraries though :(
    if I add #include "FourPieces.h" the map variable does not like that .....

--- Lab 3/FourPieces.h
+++ Lab 3/FourPieces.h
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
@@ -19,7 +22,7 @@ struct key_val
 	int value;
 };
 
-
+vector<key_val> content;
 //------------------------------------------------------------//
 //					Function Declearation					  //
 //------------------------------------------------------------//
@@ -32,7 +35,7 @@ struct key_val
 /*Initial Thoughts - https://stackoverflow.com/questions/6810656/creating-an-array-of-structs-in-c */
 /*OR just return an array of strings*/
 /*return array of strings - https://stackoverflow.com/questions/7527356/return-string-array-in-c-function */
-string* inputter(string filename);
+vector<key_val> inputter(string filename);
 
 
 /*2*/
@@ -53,3 +56,57 @@ key_val reduce(key_val collection);
 /*4*/
 //outputter takes key-value pair and outputs to the screen
 void outputter(key_val collection);
+
+//---------------------------------------------------------------------------//
+//							  Implementation								 //
+//---------------------------------------------------------------------------//
+vector<key_val> inputter(string filename)
+{
+	int word_count = 0;
+
+	ifstream input_file;
+	input_file.open(filename);
+
+	while (!input_file.eof())
+	{
+		//getline(input_file, content);
+		string temp;
+		input_file >> temp;
+
+		for (int i = 0; i < temp.size(); ++i)
+		{
+			temp[i] = tolower(temp[i]); //convert everything into lower cases
+
+			if (!((temp[i] >= 'a' && temp[i] <= 'z') || (temp[i] == '\'') || (temp[i] == '-')))
+			{
+				if (i == 0)
+				{
+					temp = temp.substr(1, temp.size());
+					temp[i] = tolower(temp[i]); //since the first character has been replaced, need to make sure it's also lower cased
+				}
+				else
+				{
+					temp[i] = ':';
+				}
+			}
+		}
+
+		size_t found = temp.find_first_of(":");
+		if (found != string::npos)
+		{
+			temp = temp.substr(0, found); //to make sure the word doesn't include other symbols 
+		}
+
+		if (temp != "\0" && temp != "-") //remove stand along character
+		{
+
+			//** https://stackoverflow.com/questions/8067338/vector-of-structs-initialization  **//
+			//Push back new key_val struct created with default constructor.
+			content.push_back(key_val());
+
+			content[word_count].key = temp;
+		}
+	}
+
+	input_file.close();
+}
